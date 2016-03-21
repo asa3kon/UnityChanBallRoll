638e73a5026c39efc8d0bb517cf5e446ac3deb00
asa3kon <asa3kon@gmail.com>
2016-03-21 16:02:42 +0900

    最初のCommit

---
+++ Assets/_MyGame_aki/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		// 差を取っておく
+		offset = this.transform.position - player.transform.position;
+	}
+	
+	// カメラの設定はLateUpdate関数内で行うこと
+	void LateUpdate () {
+		// 毎フレーム毎に加算
+		this.transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/_MyGame_aki/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+// Assert機能を利用するためのusing
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+
+
+	private Rigidbody rb;
+	private float moveHorizontal;
+	private float moveVertical;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Input系はUpdate関数内で
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		moveVertical = Input.GetAxis ("Vertical");
+	}
+
+	// RigidBody系はfixedUpdateで
+	void FixedUpdate(){
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	// 接触時に1度だけ呼ばれる
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Item"))
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
